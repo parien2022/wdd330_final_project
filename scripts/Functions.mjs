62b89b139abd96908e87ba87c18b158b7409637c
parien2022 <par22002@byui.edu>
2023-12-02 23:32:44 -0300

    Second--comit

---
+++ scripts/Functions.mjs
@@ -0,0 +1,43 @@
+
+
+export async function getTemplate(path){
+
+    const html = await fetch(path);
+    const template = await html.text();
+    return template;
+}
+
+export function insertionAdjacent(template, parentElement){
+
+    parentElement.insertAdjacentHTML("afterbegin", template);
+}
+
+//Creates and fetches both header and footer html and inserts them in DOM
+export async function creatHeaderAndFooter(headerPath, footerPath){
+
+    const headerTemplate = await getTemplate(headerPath);
+    const footerTemplate = await getTemplate(footerPath);
+
+    const headerTag = document.querySelector(".headerTag");
+    const footerTag = document.querySelector(".footerTag");
+
+    insertionAdjacent(headerTemplate, headerTag);
+    insertionAdjacent(footerTemplate, footerTag);
+
+    const button = document.querySelector(".navMenuButton");
+    responsiveNavbar(button)
+
+}
+
+
+export function responsiveNavbar(button){
+    console.log(button);
+
+    button.addEventListener('click', changeStyle);
+
+    function changeStyle(){
+        const navUl = document.querySelector('.navUl');
+        navUl.classList.toggle('responsive');
+    }
+    
+}
---
+++ scripts/Home.mjs
@@ -0,0 +1,7 @@
+import {creatHeaderAndFooter, responsiveNavbar} from './Functions.mjs';
+
+
+const header = '/wdd330_final_project/snippets/header.html';
+const footer = '/wdd330_final_project/snippets/footer.html';
+
+creatHeaderAndFooter(header, footer);
